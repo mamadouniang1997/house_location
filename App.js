b86ae3c1116e239df76ebfb965e83809d46b24a8
mamadouniang19 <niang132526@gmail.com>
2023-10-04 21:57:54 +0100

    naviagation

--- App.js
+++ App.js
@@ -1,19 +1,184 @@
-import { StatusBar } from 'expo-status-bar';
-import {SafeAreaView , StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import {
+  Image,
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
+
+import home from './assets/home.png';
+import chat from './assets/chat.png';
+import badge from './assets/badge.png';
+import intro from './assets/intro.png';
+import location from './assets/location.png';
+import notif from './assets/notif.png';
+import profil from './assets/profil.png';
+import logout from './assets/logout.png';
+import parametre from './assets/parametre.png';
+import menu from './assets/menu.png';
+import close from './assets/close.png';
 
 export default function App() {
+  const [currentTab, setCurrentTab] = useState("Home");
+  const [showMenu, setShowMenu] = useState(false);
+
+  const offsetValue = useRef(new Animated.Value(0)).current;
+  const scaleValue = useRef(new Animated.Value(1)).current;
+  const closeButtonOffset = useRef(new Animated.Value(0)).current;
+
+  
+  const tabs = [
+    { title: "Home", image: home },
+    { title: "Profile", image: profil },
+    { title: "Nearby", image: location },
+    { title: "Bookmark", image: badge },
+    { title: "Notification", image: notif },
+    { title: "Message", image: chat },
+    { title: "Setting", image: parametre },
+    { title: "Help", image: intro },
+    { title: "Logout", image: logout },
+  ];
+
   return (
-    <SafeAreaView  style={styles.container}>
-      <view  style={{ justifyContent: 'flex-start', padding: 15 }}>
-      <view>
-        {
-         // buton 
+    <SafeAreaView style={styles.container}>
+      <View style={{ flexGrow: 1, marginTop: 10 }}>
+        {tabs.map((tab, index) => (
+          <TabButton
+            key={index}
+            currentTab={currentTab}
+            setCurrentTab={setCurrentTab}
+            title={tab.title}
+            image={tab.image}
+          />
+        ))}
+      </View>
+      <Animated.View
+        style={{
+          flexGrow: 1,
+          backgroundColor: "white",
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          paddingHorizontal: 20,
+          paddingVertical: 20,
+          borderRadius: showMenu ? 19 : 0,
+
+          // Transform
+          transform: [
+            { scale: scaleValue },
+            { translateX: offsetValue }
+          ]
+        }}
+      >
+        {/* Contenu du menu */}
+        <TouchableOpacity
+          onPress={() => {
+            Animated.timing(scaleValue, {
+              toValue: showMenu ? 1 : 0.88,
+              duration: 300,
+              useNativeDriver: true
+            }).start();
+
+            Animated.timing(offsetValue, {
+              toValue: showMenu ? 0 : 230,
+              duration: 300,
+              useNativeDriver: true
+            }).start();
+
+            Animated.timing(closeButtonOffset, {
+              toValue: !showMenu ? -30 : 0,
+              duration: 300,
+              useNativeDriver: true
+            }).start();
+
+            setShowMenu(!showMenu);
+          }}
+        >
+          <Image
+            source={showMenu ? close : menu}
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: 'black',
+              marginTop: 40,
+            }}
+          />
+          <Text
+            style={{
+              fontSize: 30,
+              fontWeight: 'bold'
+            }}
+          >
+            {currentTab}
+          </Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </SafeAreaView>
+  );
+}
+
+const TabButton = ({ currentTab, setCurrentTab, title, image }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        if (title === "Logout") {
+          // Gérer le logout ici
+        } else {
+          setCurrentTab(title);
         }
-        </view>
-      <view>
-      </view>
-      </view>
-    </SafeAreaView >
+      }}
+      style={{
+        height: 60,
+        paddingTop: 70
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: 'center',
+          paddingVertical: 2,
+          backgroundColor: currentTab === title ? 'white' : 'transparent',
+          marginTop: 10,
+          height: 90,
+          paddingLeft: 20,
+          borderTopRightRadius: 32,
+          borderBottomEndRadius: 32,
+          height: 50,
+        }}
+      >
+        <Image
+          source={image}
+          style={{
+            width: 14,
+            height: 14,
+            marginTop: 5,
+            paddingHorizontal: 5,
+            justifyContent: 'center',
+            paddingVertical: 5,
+            paddingHorizontal: 5,
+            tintColor: currentTab === title ? "#0A8ED9" : 'white'
+          }}
+        />
+        <Text
+          style={{
+            fontSize: 16,
+            paddingLeft: 15,
+            lineHeight: 18,
+            borderRadius: 28,
+            paddingRight: 40,
+            overflow: 'hidden',
+            color: currentTab === title ? "#0A8ED9" : 'white'
+          }}
+        >
+          {title}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
